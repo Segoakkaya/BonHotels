52d06eaf6bb3524f2beea6c0d07295d474b3ff39
Segoakkaya <akkayasergen59@gmail.com>
2023-09-27 16:28:13 +0300

    First Update

---
+++ src/app.js
@@ -0,0 +1,9 @@
+//require("../node_modules/jquery/dist/jquery")
+window.$ = $;
+window.jQuery = $;
+jQuery = $;
+    require("../node_modules/bootstrap/dist/js/bootstrap"),
+    require("../node_modules/daterangepicker/daterangepicker"),
+    
+
+    require("./js/script");
